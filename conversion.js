594284b7981c362e8eac72f22700d84362458eac
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-20 00:36:25 +0600

    day 2 started

--- conversion.js
+++ conversion.js
@@ -1,5 +1,13 @@
-var name1 ="31";
-var name2 = "25";
-var totalName =(name1+name2);
-console.log(totalName);
+// var name1 ="31";
+// var name2 = "25";
+// var totalName =(name1+name2);
+// console.log(totalName);
 // Mathematical Operation Shorthand
+
+ var gpa ='3.5';
+ var gapNumber = parseFloat(gpa);
+ console.log(gapNumber);
+
+//  parse make sting in to integar and also in float 
+// so now we lnow how to parse 
+
