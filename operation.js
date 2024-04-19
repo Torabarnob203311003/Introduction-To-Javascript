32ddab9a2e898f5169a0cf296ccd42e21e1c5dca
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-19 21:10:17 +0600

    Simple Mathematical Operations In JavaScript

---
+++ operation.js
@@ -0,0 +1,19 @@
+// //   addition 
+//  var onionprice = 20;
+//   var eggprice = 25;
+//    var totallprice =(onionprice+eggprice);
+//    console.log(totallprice);
+
+//  subtraction
+//  var onionprice = 20;
+//   var eggprice = 25;
+//    var pricegap =(onionprice-eggprice);
+//    console.log(pricegap);
+   
+//    console.log (eggprice);
+//    console.log (onionprice);
+//    console.log (onionprice+eggprice);
+//    console.log (onionprice-eggprice);
+//    console.log (onionprice*eggprice);
+//    console.log (onionprice/eggprice);
+
