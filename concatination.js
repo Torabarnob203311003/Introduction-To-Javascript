d1dc0e0d74101aeeb7912e00981a15fa1f76f0b9
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-20 00:00:24 +0600

    math again

---
+++ concatination.js
@@ -0,0 +1,4 @@
+  var name1 = "arnob";
+   var name2 = "torab";
+    var fullname = name1+name2;
+     console.log(fullname);
---
+++ conversion.js
@@ -0,0 +1,5 @@
+var name1 ="31";
+var name2 = "25";
+var totalName =(name1+name2);
+console.log(totalName);
+// Mathematical Operation Shorthand
