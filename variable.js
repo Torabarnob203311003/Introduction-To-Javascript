62416e51d8b3303f778982183c48d075d21dd461
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-19 15:42:12 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,11 @@
+//  vary + able =variable
+// we nedd five things to declare a variable. 
+//   var(from of variable ), variable name, =, a valid number and last";" 
+
+
+var price =13;
+var age =25;
+var temperature = 41;
+var weight =65;
+var money =360;
+var day = 65;
