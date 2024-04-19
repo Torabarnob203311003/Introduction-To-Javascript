a17252caace0ee3963a1ea5a3b5bd2970c0006a4
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-20 01:06:03 +0600

    vagfol-vagshes

---
+++ reminder.js
@@ -0,0 +1,5 @@
+ var num1 =11;
+ var num2 = 2 ;
+  console.log(num1%num2);
+
+//     "/" by this we can get vagfol and by this "%" we can get vagfol;
---
+++ types.js
@@ -0,0 +1,17 @@
+//  var price = '31'
+// //  console.log(typeof price);
+//  var name = 65;
+//  console.log(typeof name);
+// //  var pass = true;
+//  console.log (typeof pass);
+
+        // so we know that by typesof we can know the types or value by terminal 
+
+
+         var num1 = 0.2;
+         var num2 = 0.1;
+         var sum =num1+num2;
+         sum = sum.toFixed(1);
+         console.log(sum);
+
+        //   so we know that bye to fixed we can make the real value of 0.1-0.2 
