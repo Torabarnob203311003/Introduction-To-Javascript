0c56dca362a68695e5a046c41a7cd03b23a08cba
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-19 16:24:05 +0600

    UPADTE LEARNING

--- variable.js
+++ variable.js
@@ -3,9 +3,23 @@
 //   var(from of variable ), variable name, =, a valid number and last";" 
 
 
+    //  ##### Variable type=Numeric ,string,Bollean ######
+//  Numeric 
 var price =13;
 var age =25;
 var temperature = 41;
 var weight =65;
 var money =360;
 var day = 65;
+
+//  string 
+ var position =" Ceo of max tube ";
+ var LOCATION = "RAJSHAI  BANGLADEH" ;
+ var  Experiecnce =" nai bollei chole "; 
+
+//   Boolean
+ var pass = true;
+  var serious = true;
+   var theif = false; 
+
+    
