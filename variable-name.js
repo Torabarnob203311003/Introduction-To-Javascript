254f21078644b6ce0005ab0dd0ef469c57545956
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-19 17:01:10 +0600

    keyWord learning

---
+++ variable-name.js
@@ -0,0 +1,9 @@
+//  1. variable  name  cannot be any keyword  
+//  2. variable name has to be in one  word . no space 
+// 3. varibale name cannot have quotation 
+// 4. varible name cannot bstarts with number but ends with the number 
+//  5. how to use long  name ----- cannot use "-"
+// we can use underscore [snake case] 
+//   also can use camle case  for long name 
+//   also can use pascale case  for long name 
+ var user_name = "bappa raj";
