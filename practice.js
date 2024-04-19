c0adc227e87fb260ad15b145280864a13fa3feb0
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-20 01:32:03 +0600

    practices problems

---
+++ practice.js
@@ -0,0 +1,30 @@
+//    problem  1 
+
+// var mommy = 1000;
+//  var apple =285 ;
+//   var orange =368;
+//    var cost = apple +orange;
+//     var shopkepperGive =mommy-cost;
+//     console.log(shopkepperGive);
+
+//   problem  2
+//     var bangla = 75.5;
+//     var english =65;
+//      var biology =85;
+//      var math = 35;
+//      var phycis = 55;
+//       var tottal = bangla+english+biology+math+phycis;
+//       var percentage = tottal/5;
+//       percentage =percentage.toFixed(2);
+//       console.log(percentage);
+ 
+// problem 3 
+//   var num1 = "my name is";
+//   var num2 = "abu Torab";
+//   console.log(num1+num2);
+
+
+//   problems 4
+//  var num1 =119;
+//  var num2 =4 ;
+//   console.log(num1 % num2);
