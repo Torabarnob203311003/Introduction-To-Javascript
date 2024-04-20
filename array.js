fcfeca8ba27a6dc935ca6c9275cf3ef247ee06d7
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-20 12:47:40 +0600

    array

---
+++ array.js
@@ -0,0 +1,10 @@
+//    var marks =[25, 15, 32, 65, 88, 99];
+//    var cash =[100,250,888,251,];
+//    var days =[9.5,3,7];
+//    console.log(marks.length,cash.length,days.length);
+
+    //  Arrays rules 3
+    //  1. array will be cover bt this "[]",
+    //  2. we have to use ", " after every IDBCursorWithValue,
+    //  3. and it will be best practice to make gap after comma ,
+    //   # we can know length of array by the .length
