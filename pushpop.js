083f70492c4484829577b11dcd6d310de3557604
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-20 14:17:50 +0600

    index update

--- pushpop.js
+++ pushpop.js
@@ -3,8 +3,9 @@
 //   console.log(numbers);
 
 //  by push and pop wee can add and remove value or element from the last of an array 
+//  by using"shift" we can  remove from start of index
 
 // var marks =[25,15,84,99,];
 //  console.log(marks);
-// var newElements = marks.pop();
+// var newElements = marks.shift();
 //   console.log(newElements);
