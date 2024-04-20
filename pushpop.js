e22dfc1b070080d5d8f549f1d8c482b9d2bb4f54
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-20 14:08:21 +0600

    push and pop

---
+++ pushpop.js
@@ -0,0 +1,10 @@
+// var numbers =['abul','kalam','azad','neel','nitin','mukesh'];
+//   numbers.pop();
+//   console.log(numbers);
+
+//  by push and pop wee can add and remove value or element from the last of an array 
+
+// var marks =[25,15,84,99,];
+//  console.log(marks);
+// var newElements = marks.pop();
+//   console.log(newElements);
