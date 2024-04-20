bdc90663f29e757dafe920d5e0e719ec0fc750f6
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-20 13:14:51 +0600

    array index

---
+++ index.js
@@ -0,0 +1,11 @@
+ var numbers =[25,45,84,97,95,65,22,45,65];
+
+//    to get element value by index 
+   var element = numbers[0];
+
+    // to add element value by index 
+    numbers[5] = 69; 
+
+    //  get index number by  the value using  ".index off" 
+    var positionIndex = numbers.indexOf(69);
+   console.log(positionIndex);
