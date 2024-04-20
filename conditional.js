ffb2c55b32ad7cb0a98b6d512d29ea5554555422
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-20 15:27:40 +0600

    conditions

---
+++ conditional.js
@@ -0,0 +1,13 @@
+ var  price =98000 ;
+ var pocket = 990;
+  if (price<pocket){
+
+
+    console.log('ulalalala')
+  }
+   else {
+
+    console.log('muri kha')
+   }
+
+    // 1st we call if then condition in first brac then sec brac nd the console the the output inside the console and the else iff the  condition idinot match just else 2nd brac console and the output  thats it 
