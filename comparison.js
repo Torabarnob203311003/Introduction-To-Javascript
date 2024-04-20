202bbc8ff5a355016a9e1035272dbc0ee957bc80
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-20 14:54:48 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,18 @@
+// console.log (5>6);
+// console.log (5<6);
+// console.log (5==6);
+// console.log (5!=6);
+// console.log (5<=6);
+// console.log (5>=60;
+
+//  var num1 = 5;
+//  var num2 = 8;
+ 
+// console.log(num1>num2);
+
+//  var num1 = 5;
+//  var num2 = 8;
+ 
+// console.log(num1<num2);
+
+                // ## we can comparison by var and by elements 
