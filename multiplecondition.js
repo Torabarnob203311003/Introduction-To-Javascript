3138ec6f4c7877031a9f0736d3b1be7c605ec117
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-21 02:13:08 +0600

    multiplestagecondition

---
+++ multiplecondition.js
@@ -0,0 +1,21 @@
+ var money = 999;
+ var danish =25;
+  var toast =15;
+  var  cake=5;
+   if (danish<money){
+
+    console.log("dan dan danish khabo")
+   }
+    else if( toast<money) {
+
+        console.log( "toast e den mama")
+    }
+     else if(cake<money) {
+
+        console.log( "cake e maybe valo hbe")
+
+     }
+     else{
+
+        console.log("batash r cha e kopal e ache maybe")
+     }
