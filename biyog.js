3d089f224a929cf9981d422b74c153382033a5f7
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-21 20:48:58 +0600

    odd

---
+++ biyog.js
@@ -0,0 +1,9 @@
+//   Target getting odd number 1-20 
+ 
+ var number = 1;
+  while (number <=20){
+
+ console.log(number);
+ number =number +2 ;
+
+  }
--- odd .js
+++
@@ -1,1 +0,0 @@
-//  target get odd numbers 1-20 
