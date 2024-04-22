05dc7fa701a119597bc24f97b568204e45e3313c
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-23 02:42:16 +0600

    continue

---
+++ contain.js
@@ -0,0 +1,14 @@
+   var elements =[12,54,66,33,22,125,44,77,88]
+
+    for ( var i = 0 ; i < elements.length; i++){
+
+    var element =  elements[i]
+
+    if(element > 70)
+     continue;
+    console.log(element);
+
+    //  it is  used skip in needed stage  
+
+
+    }
