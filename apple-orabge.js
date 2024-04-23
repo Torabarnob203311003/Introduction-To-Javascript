cbd1665e6742f6f483b2314267b6aec2a66410c3
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-23 13:24:05 +0600

    problem solving

---
+++ apple-orabge.js
@@ -0,0 +1,10 @@
+  var math =75;
+   var bangla =65;
+   var english =69;
+   var arabic =95;
+   var phycis =75;
+    var totalMark = math +bangla+english+arabic+phycis ;
+
+    console.log(totalMark);
+     var average = totalMark /5;
+     console.log(average);
