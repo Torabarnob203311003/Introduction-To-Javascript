f9b4e3461bd249ba34ef3836e0b6ebdba8470301
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-24 01:56:54 +0600

    function

---
+++ function/function.js
@@ -0,0 +1,15 @@
+//  decleartion of fuction 
+
+  function startfan(){
+  console.log('stand up');
+  console.log('walk towards the switch');
+  console.log('start the fan');
+
+  }
+
+
+//   function  calling 
+ startfan();
+ startfan();
+ startfan();
+ startfan();
