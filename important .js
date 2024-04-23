e14c3d8136c9545e1db66c68f8d8917df12a2eef
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-23 17:27:23 +0600

    important questions for the modules

---
+++ important .js
@@ -0,0 +1,43 @@
+/*
+    Variable
+    1.what is js?
+    2.how does js work?
+    3.What is variable?
+    4.Declare a variable
+    5.Types of variable ? How can you find type of a variable?
+    6.primitive and non-primitive data types?\
+    7. Naming convension of js variables?
+    8.How  will express math Operation?[+,-,/,%]
+    9.Short hand : +=,-=,*=,/= ?
+    10.++ ,-- ?
+    11.parseINT,pARSEfLOAT?
+    12.toFixed ?
+
+
+            ____ARRAY____
+            ------------
+            -------------
+            1.What is the purpose of array?
+            2.Ho to declare an array in js ?
+            3. Number of elements in an array?
+            4.What is index?
+            5.Find the  value of an element by index?
+            6.Change and elemnt by index?
+            7.Get the index of an elemnt by the value?
+            8.What does it mean when you get undefined  while geting the value of an element ?
+            9. How can ypou add  an elemnt to an array at the last position?
+            10.How can you remove an elemnt an aray?
+            11.How can you add   an element of the fast postion of ann array ?
+            12.remobve the firrst elemnt of an array?
+            
+
+            _____Conditional____
+
+            -----------------
+            ------------
+            1.Meaning of :>, < ,<=,>= !=,===,!===;
+            2.meaning of &&
+            3.meaning of ||
+            4.lets say u have x amount of money< you u have more than u have  8k then u eil by mac book , if u have more than 90k u will by gaming laptop, if u have 20k then u will by lenovo laptop if u have more than 20k then u will buys ue dlaptop, othwr wise u will use ur mobile phone ?
+            
+*/
