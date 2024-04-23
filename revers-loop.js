5b7fff1eb02f377eb5c9c145e76a99836f83f414
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-23 12:53:55 +0600

    reverse loop

---
+++ revers-loop.js
@@ -0,0 +1,16 @@
+//   var i = 10;
+//    while ( i >= 1) {
+//     i --;
+//     console.log( i )
+//    }
+
+//     reverse way in while  LOOP
+
+
+//  REVERSW WAY FOR LOOP 
+
+  for (var i = 10;i>=1 ; i-- ){
+
+ console.log(i);
+
+  }
