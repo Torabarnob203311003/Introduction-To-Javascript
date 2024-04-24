68d68dcce0cc79da5bb1d7559a7421021770b618
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-24 15:22:35 +0600

    function return

---
+++ function/return.js
@@ -0,0 +1,12 @@
+ function bringSingara ( money)
+ {
+     var singaraPrice = 10;
+     var quantity = money/ singaraPrice ;
+     return quantity;
+
+
+
+ }
+
+  var singaras = bringSingara( 250);
+  console.log('you will get :',singaras);
