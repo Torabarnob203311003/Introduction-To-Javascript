35b0efe781bfcfc6d52872fdbb623f0a624d79b1
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-24 15:00:48 +0600

    multiple peramteres

--- function/parameters.js
+++ function/parameters.js
@@ -1,8 +1,24 @@
- function bringsingara(money) {
+//  function bringsingara(money) {
 
-console.log('mama singara den');
-console.log(money);
+// console.log('mama singara den');
+// console.log(money);
 
- }
+//  }
 
-  bringsingara(150);
+//   bringsingara(150);
+
+       function sum (a,b,c,d,e) {
+      
+         var sum =a+b+c+d+e;
+       
+            console.log('the alphabet is :');
+ 
+  console .log(a,b,c,d,e);
+       }
+
+       sum ( 12,25,44,55,78);
+
+        // create function 
+        // create perameter
+        // console perameter
+        // call function
