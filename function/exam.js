660f8c3f8f6d5e5f4eac8e42656e41b36184fbd9
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-25 22:38:39 +0600

    2nd problem solved

--- function/exam.js
+++ function/exam.js
@@ -16,3 +16,29 @@
 
   //   var myaverage = make_avg (integar1,integar2,integar3) ;
   //   console.log ('my average is :', myaverage) ;
+
+
+
+//     #####   wwrite function name   make _avrg which will take an array of  three integar values and   the size of  array  and return the average of those values in js ###
+
+//   function make_avrg(arr, size) {
+//     // Check if the array length matches the specified size
+   
+
+//     // Calculate the sum of the array elements
+//     var sum = 0;
+//     for (var i = 0; i < size; i++) {
+//         sum += arr[i];
+//     }
+
+//     // Calculate the average
+//     var average = sum / size;
+
+//     // Return the average
+//     return average;
+// }
+// var array = [5, 10, 15];
+// var size = 3;
+// var average = make_avrg(array, size);
+// console.log("Average:", average);
+
