5570ebc21f4da33720a591a9f9af106650dab3ed
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-26 01:49:08 +0600

    dada inche

---
+++ function/inch-feet.js
@@ -0,0 +1,14 @@
+ function inchetofeet(inche){
+
+     const feet = inche/12;
+     return feet;
+
+ }
+
+ const dadainche = 145;
+ const dadafeet =inchetofeet(dadainche);
+ console.log('Dadafeet :', dadafeet);
+
+ const dadiinche =554;
+ const dadifeet =inchetofeet(dadiinche);
+ console.log('Dadifeet :',dadifeet);
