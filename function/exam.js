eea3b8153230a2cebb52ee2271862f46a28246e5
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-25 22:32:06 +0600

    1 problem solved

---
+++ function/exam.js
@@ -0,0 +1,18 @@
+  //   wwrite function name   make _avrg which will take an  three integar values and  n return the average
+
+  // function make_avg (integar1,integar2,integar3){
+
+  //   var total= integar1+integar2+integar3;
+  //   var average = total/3 ;
+  //    return average ;
+
+
+
+  // }
+
+  //   var integar1 = 27 ;
+  //   var integar2 = 77 ;
+  //   var integar3 = 97 ;
+
+  //   var myaverage = make_avg (integar1,integar2,integar3) ;
+  //   console.log ('my average is :', myaverage) ;
