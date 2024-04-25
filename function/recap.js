7927733eed89768f82345f5c5e6eaa611a606759
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-26 01:10:39 +0600

    recap coverd

---
+++ function/recap.js
@@ -0,0 +1,47 @@
+//    variable 
+var my name = ' khan';
+ // math operation :+-* / 
+//   short hand += -- *= /=
+
+ array ?
+  var friends = ['abul', 'kalam ','salam'];
+
+    var mythirdFriend =friends[2]
+
+
+    // 3. conditional
+    // <,>,<=,>=,==,===,!=,!==
+    // 4.loop
+     var number = 0;
+     while(number <6){
+
+        console.log(number);
+        number ++;
+     }
+
+     for(var i=0; i<5;i++){
+
+
+     }
+
+    //  5. function
+    function ismoonup(time){
+        if (time>7)
+        {
+
+            return true;
+        }
+
+
+    }
+
+    // 6.objects 
+     var jantus -{
+
+         height =60,
+         romaticism ='heavy',
+
+bapertaka = 'valoe'    }
+
+
+ 
