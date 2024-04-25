24bf05c716c6d66ed0785186da306194c5d91cad
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-26 02:25:18 +0600

    iseven

---
+++ function/iseven.js
@@ -0,0 +1,16 @@
+
+        function iseven(number){
+                const remainder = number % 2 ;
+            if( remainder === 0){
+
+                return true;
+            }
+            else{
+
+                return false ;
+
+            }
+        }
+         const thenumber = iseven(552) ;
+                  console.log (thenumber);
+
