dd51c9364545614459596cde832389151125f2c3
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-30 00:39:55 +0600

    case

---
+++ function/js-string-problems/case.js
@@ -0,0 +1,29 @@
+//   string is imuetable 
+
+//  const myName = 'KalaMandu'
+//  const myadress = 'Kalamandu'
+
+//   if (myName.toLowerCase() === myadress.toLowerCase())  {
+
+//   console.log('this is valid')
+
+//   }
+//     else {
+
+//     console.log('this is invalid')
+//   };
+ const myName = 'KalaLlu'
+ const myadress = 'Kalallu'
+
+  if (myName.toUpperCase() === myadress.toUpperCase())  {
+
+  console.log('this is valid')
+
+  }
+    else {
+
+    console.log('this is invalid')
+  };
+
+
+//    this things uses for pass sennstivety and secure perpuses  
