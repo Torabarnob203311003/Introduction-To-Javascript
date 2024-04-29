c56041d522df6f5515255530cbe711330e5687b3
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-29 19:35:32 +0600

    problem sloved

---
+++ function/sectohour.js
@@ -0,0 +1,11 @@
+//    converting  mint into hours 
+//     function mintToHour(mint){
+//             const hour = mint/60;
+//             return hour ;
+
+
+//     }
+
+//     const classtime = 720;
+//     const classtimeInhour =  mintToHour(classtime);
+//     console.log('My class Hour Is :', classtimeInhour);
