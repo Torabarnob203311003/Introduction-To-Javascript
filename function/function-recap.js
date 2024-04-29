252fb0cc2b3e95852120473b09166487876bfa84
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-29 22:08:19 +0600

    add this

---
+++ function/function-recap.js
@@ -0,0 +1,27 @@
+/*
+
+   SIX CORE CONCEPTS
+   1. variables ( declare , naming convebtion , variables type, primitive ,math operations).
+   2. condiy=tionals (comparison : >, < <=, >=, ===, !==, if -else)
+   3. loops (for , while , do-while)
+   4.Loop ( for loop, while loop,break,continue)
+   5. function (declare ,parameters,return,call, use the return value form a funxtion)
+   6. object (declare ,properties, keys, values, get prop value,loop objects)
+   */
+/*
+
+    1.google : function declaritons  vs function expressions.
+    2. google : arrow functions.
+    3. loop vs function .
+    4. array vs object
+    5. integar vs float
+    6. == vs ===
+    7. difference between : var ,let ,const
+   */
+/*
+
+    1. celsius -------> Fahrenheit 
+    2. Fahrenheit -----------> celsius
+    3. simple intresets : principal , intrest rate, time ------> intrest amount\
+    4.
+   */
