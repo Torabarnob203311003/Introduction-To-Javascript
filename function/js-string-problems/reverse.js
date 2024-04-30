5cc8df37a3735a6be57f64e6b834f928372fcbdf
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-30 20:53:16 +0600

    reverse string

---
+++ function/js-string-problems/reverse.js
@@ -0,0 +1,38 @@
+ 
+
+//   function getreverse(text){
+
+// for(let i = 0; i < text.length; i++){
+
+//         const elements = text [i];
+//         console.log (elements);
+
+
+
+// }
+
+//   }
+
+//    const myString  =' i am a good boy';
+//     const reverse = getreverse(myString);
+
+
+//   another way 
+
+ 
+
+  function getreverse(text){
+
+for(let i = text.length; i >= 0; i--){
+
+        const elements = text [i];
+        console.log (elements);
+
+
+
+}
+
+  }
+
+   const myString  =' i am a good boy';
+    const reverse = getreverse(myString);
