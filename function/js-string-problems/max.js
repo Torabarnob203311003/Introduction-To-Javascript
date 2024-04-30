9a865b9cd24abee0ce3485ebf5ae5710acf91b0f
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-30 18:51:48 +0600

    who will get the cake

---
+++ function/js-string-problems/max.js
@@ -0,0 +1,11 @@
+ const gym =95;
+ const dela = 85;
+
+ if(dela>gym){
+
+    console.log('dela will get the cake')
+ }
+ else{
+
+     console.log ('gym will get the cake')
+ }
---
+++ function/js-string-problems/max2.js
@@ -0,0 +1,11 @@
+const dela = 25;
+const gym = 35;
+const chiku = 55;
+
+if(dela > gym && dela > chiku) {
+    console.log('dela will get the cake');
+} else if(gym > dela && gym > chiku) {
+    console.log('gym will get the cake');
+} else {
+    console.log('chiku will get the cake');
+}
