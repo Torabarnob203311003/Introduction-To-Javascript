6a23a897992659114caba17750863e8e3f3a1608
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-30 18:57:27 +0600

    max with function

--- function/js-string-problems/max2.js
+++ function/js-string-problems/max2.js
@@ -1,11 +1,26 @@
-const dela = 25;
-const gym = 35;
-const chiku = 55;
+// const dela = 25;
+// const gym = 35;
+// const chiku = 55;
 
-if(dela > gym && dela > chiku) {
-    console.log('dela will get the cake');
-} else if(gym > dela && gym > chiku) {
-    console.log('gym will get the cake');
-} else {
-    console.log('chiku will get the cake');
-}
+// if(dela > gym && dela > chiku) {
+//     console.log('dela will get the cake');
+// } else if(gym > dela && gym > chiku) {
+//     console.log('gym will get the cake');
+// } else {
+//     console.log('chiku will get the cake');
+// }
+
+
+
+// write a function that will take3 numbers and will retrun the max number
+// function findMax(num1, num2, num3) {
+//     return Math.max(num1, num2, num3);
+// }
+
+// // Example usage:
+// let number1 = 10;
+// let number2 = 5;
+// let number3 = 8;
+
+// let maximum = findMax(number1, number2, number3);
+// console.log("The maximum number is:", maximum);
