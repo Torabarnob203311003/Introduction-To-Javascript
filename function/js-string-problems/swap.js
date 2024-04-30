458a216b0ae43d0ccd0494f0ff575c2f192aeadd
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-30 17:58:24 +0600

    swap done

---
+++ function/js-string-problems/swap.js
@@ -0,0 +1,13 @@
+let first = 5 ;
+  let sec = 8 ;
+  
+  console.log (first,sec);
+  
+  const temp = first;
+  first = sec;
+  sec = temp;
+  
+  console.log (first,sec);
+
+
+//   very very important 
