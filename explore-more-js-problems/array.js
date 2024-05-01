4b212e40213977bcbf88e3c8fab30df0644b8cfa
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-05-01 15:56:06 +0600

    include,concat

---
+++ explore-more-js-problems/array.js
@@ -0,0 +1,24 @@
+ var friends = [15,12,41,55];
+ var age = 15;
+ var name = 'bangladesh' ;
+  var position  = 'ceo' ;
+   var details  = {id :20, name :'fan' , waranty : 2}
+
+
+    //  how to check this thing has or not using includes 
+
+     console.log(friends.includes(41))
+
+    // how to chck  variable type 
+    console.log (typeof age )
+
+    //  how to chck array using arrayisArray 
+    console.log (Array.isArray (friends))
+     
+
+    //  how to put 2 add aray in a array  using  concat
+    
+    const newfriends = [69,99,85,78]
+    const allfrineds = newfriends.concat(friends)
+    console.log(allfrineds);
+ 
