5a5aff8baae584e800056f50e1beecbbf5ef3141
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-05-01 16:19:49 +0600

    slice

---
+++ explore-more-js-problems/slice.js
@@ -0,0 +1,5 @@
+const numbers = [25,75,85,74,89,77]
+const partisal = numbers.slice(2,5)
+console.log(partisal);
+
+//  by using slice we can  get array by our want   we have to use slice and the index from where to  the last elemnts 
