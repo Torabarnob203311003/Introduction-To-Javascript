59609815c0d2788f8566b03a035ecaea9547e9fc
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-05-03 21:47:18 +0600

    factorial

--- js-core-concept-bonus/simple.js
+++ js-core-concept-bonus/simple.js
@@ -1,7 +1,17 @@
- let sum =0;
+//  let sum =0;
 
- for ( let i = 0; i <=5; i++){
+//  for ( let i = 0; i <=5; i++){
 
-     sum = sum+i;
- }
- console.log(sum);
+//      sum = sum+i;
+//  }
+//  console.log(sum);
+
+
+//  let sum =0;
+//  for ( let i = 5; i >=0; i--){
+ 
+//       sum = sum+i;
+//   }
+//   console.log(sum);
+
+//  fucntioner vitore theke jdi call kri sei function k take recursion bole 
