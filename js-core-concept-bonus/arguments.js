eaec016249c9cb6c26bce1b37855f182396dd33c
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-05-03 23:18:38 +0600

    arguments

---
+++ js-core-concept-bonus/arguments.js
@@ -0,0 +1,8 @@
+ function add(num1,num2){
+
+        console.log (num1,num2);
+        console.log(arguments[4]);    //arguments just like an array but not an array.. its an array like object.. we cannot do any changed by psuh and pop or others things like any array 
+
+ }
+
+ add(12,15,55,85,77);
