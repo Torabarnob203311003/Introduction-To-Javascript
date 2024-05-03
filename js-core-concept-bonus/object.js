d3f797e6dec48da31dc6b27f705c947d029afa9d
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-05-03 22:13:57 +0600

    object

---
+++ js-core-concept-bonus/object.js
@@ -0,0 +1,27 @@
+  const nayok = {
+        name : 'sakib khan',
+        age : 20,
+         isSingle : true,
+         adress : 'movie thearter of dhaka',
+         friends : ['raj','arnob','shihab'],
+
+         act: function(){
+
+             console.log ('acting like sakib khan')
+         },
+         car :  {
+                     name : 'tesla',
+                     price :5000000,
+                     color :'red',
+                     model : '2020',
+                     manufactirie : {
+
+                        name :'tesla',
+                        country : 'USA',
+                     }
+
+         }
+
+  }
+  console.log(nayok.act);
+nayok.act()
