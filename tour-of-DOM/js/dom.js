81f9a3927d9499e9acfc8933c8af27a05e5bc725
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-05-04 17:16:56 +0600

    all dom travelsing

--- tour-of-DOM/js/dom.js
+++ tour-of-DOM/js/dom.js
@@ -3,3 +3,18 @@
 
  const Licollection = documnets.getelemetsbyTagname('li');
  console.log(Licollection);
+
+//    option 1 :
+//     get elementsByTagname
+
+//    option 2 :
+//     get elementById
+
+//    option 3 :
+//     get elementsByclassname
+
+//    option 4 :
+//     get elemntsBySelector
+
+//    option 5 :
+//     get elementsBySelectorAll
