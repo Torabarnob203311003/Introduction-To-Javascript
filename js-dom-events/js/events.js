cd1aae34918d6aee3d6903b6acc0bf3acc45b5ec
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-05-04 21:17:42 +0600

    Onclick Handler Directly Or Via Javascript,

---
+++ js-dom-events/js/events.js
@@ -0,0 +1,25 @@
+
+
+//  option number 1 
+//   direct use in click me Option
+
+
+//  Option 2
+
+  function makePurple(){
+
+    document.body.style.backgroundColor='purple';
+
+  };
+
+// //    create a function and call function in onclick
+ //option 3
+
+   const makeBlackButoon = document .getElementById('make-black');
+   makeBlackButoon.onclick = makeBlack;
+   
+   function makeBlack(){
+    document.body.style.backgroundColor='black';
+   }
+    
+
