b9e2fae1e159dacdf1b85ed6ad0d516779def1a6
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-05-19 23:10:06 +0600

    recap of Dom Manupulation and Eventhandeler

---
+++ tour-of-DOM/es6/js/dom.js
@@ -0,0 +1,27 @@
+document.getElementById('btn').addEventListener('click',function(){
+
+  const frnd = document.getElementsByClassName('friends')
+for(const friend of frnd){
+
+    friend.style.backgroundColor = 'lightblue';
+}
+
+});
+
+ document.getElementById('btn-center').addEventListener('click',function(){
+    const thrid = document.getElementById('third-friend')
+    thrid.style.textAlign ='center';
+//  console.log(thrid) 
+
+ });
+
+ document.getElementById('btn-add').addEventListener('click',function(){
+
+   const frndcontainer = document.getElementById('friends')
+ const friend = document.createElement('div');
+ friend.innerHTML = `<h3>hi i am new friend </h3>
+ <p> what is your name</p> `
+  frndcontainer.appendChild(friend)
+
+
+ })
