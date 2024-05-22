5bf5b8c8f37be69ee8ae13f941b8c26e39b7c628
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-05-23 01:05:03 +0600

    filter

---
+++ tour-of-DOM/es6/js/filter.js
@@ -0,0 +1,4 @@
+//filter selcets elements on a condtion and return an array woith full the conditions //
+ const players =  [55,65,45,87,63,66,69,80]
+ const selected = players.filter(n=>n<40)
+ console.log(selected)
