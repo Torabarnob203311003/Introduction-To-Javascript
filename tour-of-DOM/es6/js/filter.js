d31e13b82ed037b5858c860108334bb3e17b1516
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-05-23 01:10:02 +0600

    find

--- tour-of-DOM/es6/js/filter.js
+++ tour-of-DOM/es6/js/filter.js
@@ -1,4 +1,5 @@
 //filter selcets elements on a condtion and return an array woith full the conditions //
+//  and empty output it return and empty array 
  const players =  [55,65,45,87,63,66,69,80]
  const selected = players.filter(n=>n<40)
  console.log(selected)
---
+++ tour-of-DOM/es6/js/find.js
@@ -0,0 +1,5 @@
+//filter selcets elements on a condtion and return 1st  ouput full the conditions //
+//  its return undefined 
+ const players =  [55,65,45,87,63,66,69,80]
+ const selected = players.find(n=>n<40)
+ console.log(selected)
