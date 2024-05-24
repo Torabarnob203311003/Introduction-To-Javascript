31b6f3cb9aeac5088bd368243bf2c4846e420e0b
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-05-25 02:05:00 +0600

    js data types

---
+++ common-js/types.js
@@ -0,0 +1,9 @@
+//  js i s a dynamic langugage
+
+  //primitive which   has sinagle value, doesnot need to store multiple value
+  // non-primitive which has to store or assgin multiple value 
+
+
+  // others language are strict in data types like C,c++
+
+  // THERE are also some others special types ....
