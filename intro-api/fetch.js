f0383df9e023ccbfc8ed0db0325340ddc25a37ef
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-05-26 01:31:59 +0600

    load more data

--- intro-api/fetch.js
+++ intro-api/fetch.js
@@ -14,3 +14,23 @@ fetch(url)
 
 
 }
+function loadData2(){
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+fetch(url)
+.then(response => response.json())
+.then(json=>console.log(json)) 
+
+
+}
+
+ function loadUsers(){
+        fetch ('https://jsonplaceholder.typicode.com/users')
+        .then (res=> res.json())
+        .then (data=> another(data))
+ }
+
+ function another (data){
+
+  console.log(data);
+
+ }
