d8a0c626aeae0be8dc1cb4b56075336296d3fcb1
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-06-13 23:29:22 +0600

    added clone method

---
+++ common-js/objectsofjs/mtehod.js
@@ -0,0 +1,37 @@
+ // create object using literals
+  const player = {}
+
+    player.name =' small nirob'
+    player.specialty = 'batsman'
+    console.log (player)
+   // 2. object cinstructor 
+    const person = new object ();
+    console.log(person)
+    //3. object create method
+
+     const atel = object .create ( student)
+     console.log (atel.name)
+     // 4. class 
+      class person {
+
+
+        name : 'khaelk';
+        adress = 'dhaka'
+;    
+
+  constructor ( age){
+
+    this.age = age
+  }
+
+  }
+
+//5. function diya object  
+
+
+function car ( model, price){
+
+
+     this.model = model;
+     this.price = price ;
+}
