7d7d354aa11e7015494c02abc0eea00294a74ceb
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-06-23 22:15:37 +0600

    loop for js

---
+++ common-js/objectsofjs/againloop.js
@@ -0,0 +1,14 @@
+ const keys = [12,2,55,47,88]
+ 
+ 
+  //array er opro for of
+ for(const key of keys){
+
+
+
+  }
+// for loop in object er opor e
+   for ( const key in bottle){
+
+    cosnole.log(key)
+   }
