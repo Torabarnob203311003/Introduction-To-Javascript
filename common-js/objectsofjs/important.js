592195210ddd604b43f75a052c7986fe415303be
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-06-24 17:37:26 +0600

    important

---
+++ common-js/objectsofjs/important.js
@@ -0,0 +1,9 @@
+// shallow copy ?
+
+
+ const  data= { alice,' 23', nura }
+ const secData = (...data) // this shallow copy
+ console.log(secData)
+
+ // if u frewxw we cantno create or upadte or delet
+ // if we seal wi cannot create or delete but we can update
