669402ae214300a481b06c32970c8587db6f3e57
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-06-25 02:36:16 +0600

    taking preparation for react js

---
+++ common-js/js-concepts-for-react/fundameentals.js
@@ -0,0 +1,40 @@
+//how to deaclare a variable using let and const?
+// if cahngeable thn use let other wise use const
+
+// 6 basic conditions
+// multiple conditions && and ||
+ 
+// 3. array declare 
+const numers = [45,85,96]
+numers [0]=99;
+// index,
+//length,push
+
+//4. for loop
+for(let i=0 ; i<numbers.length; i++){
+
+    comnst number = numbers [i]
+    console log(number)
+}
+//5. functions
+function multipy (num1, num2){
+ const result = num1*num2;
+ return result ;
+
+}
+const output = multipy(35,25)
+
+// objects 
+//acess property by name 3 ways
+
+const student {
+
+    name :' sakib al hasan',
+    age: 32,
+    movies : ['king jhan' , 'god is one']
+}
+
+console.log ( student.age)// direct way
+
+console.log ( student['age'])//  acces via property
+
