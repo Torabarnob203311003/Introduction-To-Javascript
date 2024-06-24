ff2a2a795949854640c91b88394a2f7d30b6884a
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-06-25 02:46:59 +0600

    templet string recap for react js

---
+++ common-js/js-concepts-for-react/es6.js
@@ -0,0 +1,28 @@
+const student {
+
+    name :' sakib al hasan',
+    age: 32,
+    movies : ['king jhan' , 'god is one']
+}
+//1.templet string
+const about =` my name is ${student.name} age is ${student.age}`
+//2. arrow function
+
+ const getFifthyFive =() => 55;
+ const addDicyFive = num=> number+65;
+ const ISEven= X => X%2==0;
+ const addThree = (x,y,z)=> x+y+z;
+ const doMah = (num,num2)=>{
+
+
+const sum = num1+num2;
+    return sum ;
+
+ }
+
+ //speard operator
+
+  const newNumbers = [...againnumbers];
+  numbers.push(99)
+  //create a new array from and older array and add an element
+  const currentNumbers = [...numbers,55]
