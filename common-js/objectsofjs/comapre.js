dd57daf59ce4c8d9f5e14e1a0f31ca2146a4d2cf
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-06-24 15:29:55 +0600

    comapre

---
+++ common-js/objectsofjs/comapre.js
@@ -0,0 +1,10 @@
+ const first = {}
+ const second = {}
+ if ( first===second){ // === alwys chck value and types and references
+
+    console.log('thery are same')
+ }
+ else{
+
+    console.log('different')
+ }
